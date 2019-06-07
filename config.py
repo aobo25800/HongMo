a904c200b550546e8dd57a8cad79af4218226750
Da Zhi <37598262@qq.com>
2019-06-07 23:47:35 +0800

    定义配置文件实现状态保持到redis数据库中

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
+from redis import StrictRedis
 class Config:
     DEBUG = True
+    # 设置秘钥
+    SECRET_KEY = 'Px3gNC9PMay6Tb8SUxmHB0QDHNrPRSmeih66p4uc3p0WrTwByQmuba96Yfufmw45roI='
+
+    # 配置状态保持中的session信息，储存在redis数据库中
+    SESSION_TYPE = 'redis'
+    # session链接redis数据库实例
+    SESSION_REDIS = StrictRedis(host='127.0.0.1', port=6379)
+    # session签名
+    SESSION_USE_SIGNER = True
+    # session有效期
+    PERMANENT_SESSION_LIFETIME = 86400
     
--- manage.py
+++ manage.py
@@ -1,11 +1,17 @@
-from flask import Flask
-
+from flask import Flask, session
+# 导入flask_session扩展包，用来指定session信息储存的位置
+from flask_session import Session
+# 导入配置对象
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
+Session(app)
 @app.route('/')
 def index():
+    session['hongmo'] = '2018'
     return "HongMo"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
